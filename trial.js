cb4102a3d19ccc635d1c4bfc9d7cdb456c4d72e6
Arvind Kandhare <arvind.kandhare@oracle.com>
2019-01-29 10:51:05 -0800

    Got the spehro API working on mac.

--- trial.js
+++ trial.js
@@ -4,6 +4,7 @@ var sphero = require("sphero"),
 
 bb8.connect(function() {
   // roll BB-8 in a random direction, changing direction every second
+  console.log("Connected")
   setInterval(function() {
     var direction = Math.floor(Math.random() * 360);
     bb8.roll(150, direction);
